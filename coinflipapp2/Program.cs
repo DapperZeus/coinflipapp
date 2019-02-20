96bce03e2862916eb4c9a0bf683103ee7dddaecb
DapperZeus <33703306+DapperZeus@users.noreply.github.com>
2019-02-20 14:58:14 -0700

    c

--- coinflipapp2/Program.cs
+++ coinflipapp2/Program.cs
@@ -6,7 +6,30 @@ namespace coinflipapp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int coin;
+            string userGuess;
+            Random rng = new Random();
+
+            Console.Write("Heads or Tails, sir?");
+            userGuess = Console.ReadLine();
+
+            coin = rng.Next(0,2);
+
+            if (coin == 0 && userGuess == "Tails")
+            {
+                Console.WriteLine("Winner winner chicken dinner");
+            }
+            else if (coin == 1 && userGuess == "Heads")
+            {
+                Console.WriteLine("Winner winner chicken dinner");
+            }
+            else
+            {
+                if (coin == 0)
+                {
+                    Console.WriteLine("Tails. It really be like that sometimes");
+                }
+            }
         }
     }
 }
